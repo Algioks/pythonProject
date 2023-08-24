5bd9b8de3065f6131dd96551c4e6b2fa5c44240f
Algirdas <algirdas.arlauskas1@gmail.com>
2023-08-24 14:15:46 +0300

    a

---
+++ test.py
@@ -0,0 +1,11 @@
+
+
+def test_add_two_numbers_possitive():
+    input_1=1
+    input_2=4
+    
+    output = add_two_numbers(input_1, input_2)
+    
+    assert output == 5, "method failed"
+    
+    
